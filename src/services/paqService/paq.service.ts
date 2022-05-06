58e0d09092691c4aee0aa3dacb6903d864a20be3
danieltwork <46277472+danieltwork@users.noreply.github.com>
2022-05-06 15:49:19 +0200

    Remove unnecessary console.log

--- src/services/paqService/paq.service.ts
+++ src/services/paqService/paq.service.ts
@@ -8,7 +8,6 @@ function push(collection: any[]) {
 
   (window as PiwikProWindow)._paq.push(["setCustomUrl", window.location.pathname]);
   (window as PiwikProWindow)._paq.push(["setDocumentTitle", document.title]);
-  console.log(process.env)
   IS_DEBUG && console.log('Push', collection);
   return (window as PiwikProWindow)._paq.push(collection);
 }
