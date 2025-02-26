9ac00d20b223adcc4bba675d83d2f88a57e860e9
Michał Warać <49625375+auto200@users.noreply.github.com>
2025-02-26 11:07:27 +0100

    Set provider upon initialization (#52)
    * set provider upon initialization
    
    * bump tracking base lib
    
    * bump version

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-// NOTE: .tsx file extension is required for microbundle-crl to pick up and build the lib
-export { default } from '@piwikpro/tracking-base-library'
+import * as PiwikPRO from '@piwikpro/tracking-base-library'
 export * from '@piwikpro/tracking-base-library'
+import { version } from '../package.json'
+import { Miscellaneous, Initialize } from '@piwikpro/tracking-base-library'
 
-// NOTE: more react specific functionalities will be added here
+const initialize: Initialize = (...args) => {
+  if (typeof window !== 'undefined') {
+    Miscellaneous.setTrackingSourceProvider('react', version)
+  }
+  
+  PiwikPRO.default.initialize(...args)
+}
+
+export default {
+  ...PiwikPRO.default,
+  initialize
+  // fixes some 'The inferred type of 'default' cannot be named without a reference to ...' error
+} as typeof PiwikPRO.default
