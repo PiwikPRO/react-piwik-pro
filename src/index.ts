d29d88ddf4591c236fca7b1de456fa5389f41a4c
Ashley <a.tarczynski-borkowicz@piwik.pro>
2026-06-30 11:32:29 +0200

    chore: bump minimatch and other deps

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,9 @@ const initialize: Initialize = (...args) => {
   PiwikPRO.default.initialize(...args)
 }
 
+// fixes some 'The inferred type of 'default' cannot be named without a reference to ...' error
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion -- required for portable .d.ts emit
 export default {
   ...PiwikPRO.default,
   initialize
-  // fixes some 'The inferred type of 'default' cannot be named without a reference to ...' error
 } as typeof PiwikPRO.default
